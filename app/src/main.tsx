4245606be4e708ea74670462101456c2f391f3c7
Steve Miles <42913094+SteveMilesQuant@users.noreply.github.com>
2023-04-28 18:07:06 -0400

    add theme to ensure chakra light mode in dev builds

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}>
-    <React.StrictMode>
-      <ChakraProvider>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <GoogleOAuthProvider
+        clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}
+      >
         <App />
-      </ChakraProvider>
-    </React.StrictMode>
-  </GoogleOAuthProvider>
+      </GoogleOAuthProvider>
+    </ChakraProvider>
+  </React.StrictMode>
 );
