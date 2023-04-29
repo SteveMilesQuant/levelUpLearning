b59fa333a81a77344604d42bb8f7bd84b9a79b03
Steve Miles <42913094+SteveMilesQuant@users.noreply.github.com>
2023-04-29 10:03:32 -0400

    fix popover warning in edit button

--- app/src/components/EditButton.tsx
+++ app/src/components/EditButton.tsx
@@ -12,6 +12,7 @@ import {
   PopoverTrigger,
   PopoverFooter,
   HStack,
+  Box,
 } from "@chakra-ui/react";
 
 interface Props {
@@ -29,49 +30,52 @@ const EditButton = ({
   onUpdate,
   onClose,
 }: Props) => (
-  <Popover>
-    {({ onClose: onClosePopover }) => (
-      <>
-        <PopoverTrigger>
-          <ActionButton Component={AiFillEdit} label="Edit" />
-        </PopoverTrigger>
-        <PopoverContent>
-          <PopoverArrow />
-          <PopoverHeader>
-            <Text>
-              <strong>{title}</strong>
-            </Text>
-            <PopoverCloseButton />
-          </PopoverHeader>
-          <PopoverBody>{children}</PopoverBody>
-          <PopoverFooter>
-            <HStack spacing={2} justifyContent="right">
-              <Button
-                onClick={() => {
-                  onClosePopover();
-                  onClose();
-                }}
-              >
-                Cancel
-              </Button>
-              <Button
-                onClick={() => {
-                  onUpdate();
-                  if (!holdOpen) {
+  <Box>
+    {/* Put popover in a box, to avoid warnings about it, when container might try to apply css*/}
+    <Popover>
+      {({ onClose: onClosePopover }) => (
+        <>
+          <PopoverTrigger>
+            <ActionButton Component={AiFillEdit} label="Edit" />
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverHeader>
+              <Text>
+                <strong>{title}</strong>
+              </Text>
+              <PopoverCloseButton />
+            </PopoverHeader>
+            <PopoverBody>{children}</PopoverBody>
+            <PopoverFooter>
+              <HStack spacing={2} justifyContent="right">
+                <Button
+                  onClick={() => {
                     onClosePopover();
                     onClose();
-                  }
-                }}
-                bgColor="blue.300"
-              >
-                Update
-              </Button>
-            </HStack>
-          </PopoverFooter>
-        </PopoverContent>
-      </>
-    )}
-  </Popover>
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  onClick={() => {
+                    onUpdate();
+                    if (!holdOpen) {
+                      onClosePopover();
+                      onClose();
+                    }
+                  }}
+                  bgColor="blue.300"
+                >
+                  Update
+                </Button>
+              </HStack>
+            </PopoverFooter>
+          </PopoverContent>
+        </>
+      )}
+    </Popover>
+  </Box>
 );
 
 export default EditButton;
