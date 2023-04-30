1abbf0f00acf833a6a6ccaeb8a3641a62a127c2b
Steve Miles <42913094+SteveMilesQuant@users.noreply.github.com>
2023-04-30 10:27:55 -0400

    fixed delete program

--- app/src/programs/components/ProgramForm.tsx
+++ app/src/programs/components/ProgramForm.tsx
@@ -1,7 +1,6 @@
 import { HStack } from "@chakra-ui/react";
 import useProgramForm from "../hooks/useProgramForm";
 import ProgramFormBody from "./ProgramFormBody";
-import programService from "../program-service";
 import { Program } from "../Program";
 import { useNavigate } from "react-router-dom";
 import ActionButton from "../../components/ActionButton";
@@ -10,6 +9,7 @@ import { AiFillEdit } from "react-icons/ai";
 import CancelButton from "../../components/CancelButton";
 import SubmitButton from "../../components/SubmitButton";
 import { useState } from "react";
+import { useDeleteProgram } from "../hooks/usePrograms";
 
 interface Props {
   program?: Program;
@@ -20,16 +20,9 @@ const ProgramForm = ({ program, canUpdate }: Props) => {
   const navigate = useNavigate();
   const [isEditing, setIsEditing] = useState(false);
   const programForm = useProgramForm(program);
-
-  const handleDeleteProgram = () => {
-    if (!program) return;
-    programService
-      .delete(program.id)
-      .then(() => {
-        navigate("/programs");
-      })
-      .catch((err) => console.log(err));
-  };
+  const deleteProgram = useDeleteProgram(() => {
+    navigate("/programs");
+  });
 
   return (
     <>
@@ -42,7 +35,10 @@ const ProgramForm = ({ program, canUpdate }: Props) => {
             onClick={() => setIsEditing(true)}
             disabled={isEditing}
           />
-          <DeleteButton onConfirm={handleDeleteProgram} disabled={isEditing}>
+          <DeleteButton
+            onConfirm={() => deleteProgram.mutate(program?.id)}
+            disabled={isEditing}
+          >
             {program?.title}
           </DeleteButton>
           <CancelButton
--- app/src/programs/program-service.ts
+++
@@ -1,4 +0,0 @@
-import create from "../services/http-service";
-import { ProgramData, Program } from "./Program";
-
-export default create<ProgramData, Program>("/programs");
