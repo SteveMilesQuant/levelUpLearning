f01571b3848281244d042c5a156ff6c555923cba
Steve Miles <42913094+SteveMilesQuant@users.noreply.github.com>
2023-05-08 20:34:41 -0400

    create "under construction" home page

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,5 +1,23 @@
+import { SimpleGrid, Icon, Text, Container } from "@chakra-ui/react";
+import { IoConstructOutline } from "react-icons/io5";
+
 const Home = () => {
-  return <></>;
+  return (
+    <SimpleGrid columns={1} spacing={5}>
+      <Container centerContent>
+        <Icon as={IoConstructOutline} boxSize="200px" />
+      </Container>
+      <Container centerContent>
+        <Text fontSize="xl">
+          Welcome to Level Up Writing! We are currently under construction, but
+          feel free to log in with Google and peruse the site. We are on a
+          temporary SQL server, so your information will be deleted at some
+          point, but if you need it done sooner, email Steve at
+          steven.miles.quant@gmail.com.
+        </Text>
+      </Container>
+    </SimpleGrid>
+  );
 };
 
 export default Home;
