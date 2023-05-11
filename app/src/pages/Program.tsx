06e89834e6d02a2100615f3a58c798c9d9a27ac9
Steve Miles <42913094+SteveMilesQuant@users.noreply.github.com>
2023-05-11 19:50:59 -0400

    refactor: make LevelList component

--- app/src/pages/Program.tsx
+++ app/src/pages/Program.tsx
@@ -1,62 +1,21 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {
-  Box,
-  Button,
-  HStack,
-  List,
-  ListItem,
-  Stack,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import PageHeader from "../components/PageHeader";
-import {
-  Level,
-  useLevels,
-  ProgramForm,
-  LevelForm,
-  LevelFormModal,
-  useProgram,
-} from "../programs";
-import ListButton from "../components/ListButton";
+import { ProgramForm, useProgram } from "../programs";
+import LevelList from "../programs/components/LevelList";
 
 const Program = () => {
   const { id: idStr } = useParams();
   const id = idStr ? parseInt(idStr) : undefined;
 
-  const [selectedLevel, setSelectedLevel] = useState<Level | undefined>(
-    undefined
-  );
+  const { data: program, error, isLoading } = useProgram(id);
 
-  const {
-    data: program,
-    error: programError,
-    isLoading: loadingProgram,
-  } = useProgram(id);
-  const {
-    data: levels,
-    error: levelsError,
-    isLoading: loadingLevels,
-  } = useLevels(id);
-
-  useEffect(() => {
-    if (levels) setSelectedLevel(levels[0]);
-  }, [!!levels]);
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  if (loadingProgram || loadingLevels || !program || !levels) return null;
-  if (programError) throw programError;
-  if (levelsError) throw levelsError;
+  if (isLoading) return null;
+  if (error) throw error;
 
   return (
     <>
-      <PageHeader label={program?.title} hideUnderline={true}></PageHeader>
+      <PageHeader label={program.title} hideUnderline={true}></PageHeader>
       <Tabs variant="enclosed">
         <TabList>
           <Tab>Program</Tab>
@@ -67,44 +26,10 @@ const Program = () => {
             <ProgramForm program={program} isReadOnly={false} />
           </TabPanel>
           <TabPanel>
-            <HStack alignItems="start" spacing={10}>
-              <Stack spacing={3}>
-                {levels
-                  ?.sort((a, b) => a.list_index - b.list_index)
-                  .map((level) => (
-                    <ListButton
-                      key={level.id}
-                      isSelected={selectedLevel?.id === level.id}
-                      onClick={() => setSelectedLevel(level)}
-                    >
-                      {level.list_index + ": " + level.title}
-                    </ListButton>
-                  ))}
-                <Button onClick={onOpen}>Add level</Button>
-              </Stack>
-              <Box width="100%">
-                {levels
-                  ?.filter((level) => level.id === selectedLevel?.id)
-                  .map((level) => (
-                    <LevelForm
-                      key={level.id}
-                      programId={program.id}
-                      level={level}
-                      isReadOnly={false}
-                    ></LevelForm>
-                  ))}
-              </Box>
-            </HStack>
+            <LevelList programId={program.id} />
           </TabPanel>
         </TabPanels>
       </Tabs>
-
-      <LevelFormModal
-        title="Add level"
-        isOpen={isOpen}
-        onClose={onClose}
-        programId={program.id}
-      ></LevelFormModal>
     </>
   );
 };
---
+++ app/src/programs/components/LevelList.tsx
@@ -0,0 +1,66 @@
+import { Box, Button, HStack, Stack, useDisclosure } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Level } from "../Level";
+import useLevels from "../hooks/useLevels";
+import ListButton from "../../components/ListButton";
+import LevelForm from "./LevelForm";
+import LevelFormModal from "./LevelFormModal";
+
+interface Props {
+  programId: number;
+}
+
+const LevelList = ({ programId }: Props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedLevel, setSelectedLevel] = useState<Level | undefined>(
+    undefined
+  );
+  const { data: levels, error, isLoading } = useLevels(programId);
+  useEffect(() => {
+    if (levels) setSelectedLevel(levels[0]);
+  }, [!!levels]);
+
+  if (isLoading) return null;
+  if (error) throw error;
+
+  return (
+    <>
+      <HStack alignItems="start" spacing={10}>
+        <Stack spacing={3}>
+          {levels
+            ?.sort((a, b) => a.list_index - b.list_index)
+            .map((level) => (
+              <ListButton
+                key={level.id}
+                isSelected={selectedLevel?.id === level.id}
+                onClick={() => setSelectedLevel(level)}
+              >
+                {level.list_index + ": " + level.title}
+              </ListButton>
+            ))}
+          <Button onClick={onOpen}>Add level</Button>
+        </Stack>
+        <Box width="100%">
+          {levels
+            ?.filter((level) => level.id === selectedLevel?.id)
+            .map((level) => (
+              <LevelForm
+                key={level.id}
+                programId={programId}
+                level={level}
+                isReadOnly={false}
+              ></LevelForm>
+            ))}
+        </Box>
+      </HStack>
+      <LevelFormModal
+        title="Add level"
+        isOpen={isOpen}
+        onClose={onClose}
+        programId={programId}
+      ></LevelFormModal>
+    </>
+  );
+};
+
+export default LevelList;
