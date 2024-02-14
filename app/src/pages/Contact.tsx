b5e031403ab03821aaf2d42562c76fa0021d68dd
Steve Miles <steven.miles.quant@gmail.com>
2024-02-14 18:39:47 -0500

    update contact page with new location

--- app/src/pages/Contact.tsx
+++ app/src/pages/Contact.tsx
@@ -15,7 +15,7 @@ const Contact = () => {
           Location: As a new small business, we are currently working toward
           attaining a permanent establishment in the Apex area. For now, we are
           renting out spaces from local venues including the Apex Campus at Hope
-          Community Church.
+          Community Church and Apex Baptist Church.
         </Text>
       </Stack>
     </BodyContainer>
