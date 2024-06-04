48355c961fa25c0f02122f04ea608f14e84184b0
Steve Miles <steven.miles.quant@gmail.com>
2024-06-04 18:26:48 -0400

    format events spinner

--- app/src/pages/Events.tsx
+++ app/src/pages/Events.tsx
@@ -1,12 +1,12 @@
 import BodyContainer from "../components/BodyContainer";
-import { Divider, Spinner, Stack } from "@chakra-ui/react";
+import { Box, Divider, Spinner, Stack } from "@chakra-ui/react";
 import { EventPublic, useEvents } from "../events";
 import { Fragment } from "react";
 
 const Events = () => {
   const { data: events, error, isLoading } = useEvents();
   if (error) throw error;
-  if (isLoading) return <Spinner />
+  if (isLoading) return <BodyContainer><Box width="100%"><Box marginX="auto" width='fit-content'><Spinner size="xl" /></Box></Box></BodyContainer>;
 
   return (
     <BodyContainer>
