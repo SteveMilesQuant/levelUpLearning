72b56da687b10229fd79dd9d69462422534779d7
Steve Miles <steven.miles.quant@gmail.com>
2024-09-10 17:38:34 -0400

    new homepage carousel images

--- app/src/components/HomePageCarousel.tsx
+++ app/src/components/HomePageCarousel.tsx
@@ -3,20 +3,18 @@ import { Carousel } from "react-responsive-carousel";
 import carouselImage1 from "../assets/c1.svg";
 import carouselImage2 from "../assets/c2.svg";
 import carouselImage3 from "../assets/c3.svg";
-//import carouselImage4 from "../assets/c4.svg";
+import carouselImage4 from "../assets/c4.svg";
 import carouselImage5 from "../assets/c5.svg";
 import carouselImage6 from "../assets/c6.svg";
-import carouselImage7 from "../assets/c7.svg";
 
 const HomePageCarousel = () => {
     const cards = [
         { id: 0, img: carouselImage1 },
         { id: 1, img: carouselImage2 },
         { id: 2, img: carouselImage3 },
-        // { id: 3, img: carouselImage4 },
+        { id: 3, img: carouselImage4 },
         { id: 4, img: carouselImage5 },
         { id: 5, img: carouselImage6 },
-        { id: 6, img: carouselImage7 },
     ];
 
     return (
