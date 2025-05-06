956ab6b6a689ac5cd346ecbd17f70bc0f589b8e3
Steve Miles <steven.miles.quant@gmail.com>
2025-05-05 20:23:50 -0400

    make nav bar sticky

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { useBreakpointValue } from "@chakra-ui/react";
+import { Box, useBreakpointValue } from "@chakra-ui/react";
 import NavBarMobile from "./NavBarMobile";
 import NavBarDesktop from "./NavBarDesktop";
 
 const NavBar = () => {
   const navbar = useBreakpointValue({ base: <NavBarMobile />, xl: <NavBarDesktop /> });
-  return navbar || null;
+  return <Box width="full" position="sticky" top="0"
+    zIndex="sticky">{navbar || null}</Box>;
 };
 
 export default NavBar;
