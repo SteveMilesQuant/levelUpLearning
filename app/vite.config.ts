0ec4a628a5df0e912478a2cbc6cbf2d7744bb6c0
Steve Miles <steven.miles.quant@gmail.com>
2025-07-26 15:22:39 -0400

    adjust CI/CD process to make dev side easier

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    watch: {
+      usePolling: mode === 'development',
+    },
+  },
+}))
