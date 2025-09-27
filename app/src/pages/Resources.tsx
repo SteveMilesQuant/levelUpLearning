cec69fb1532e260baf81a5eab5564fee9dfa609c
Steve Miles <steven.miles.quant@gmail.com>
2025-09-27 10:55:19 -0400

    Add link to resources for "best summer camps"

--- app/src/pages/Resources.tsx
+++ app/src/pages/Resources.tsx
@@ -1,7 +1,6 @@
 import {
   Box,
   Heading,
-  LinkBox,
   Spinner,
   Stack,
   Tab,
@@ -13,7 +12,6 @@ import {
 import BodyContainer from "../components/BodyContainer";
 import PageHeader from "../components/PageHeader";
 import { useResourceGroups } from "../resources";
-import { Link as RouterLink } from "react-router-dom";
 import TextButton from "../components/TextButton";
 
 const Resources = () => {
@@ -24,31 +22,38 @@ const Resources = () => {
   return (
     <BodyContainer>
       <PageHeader>Resources</PageHeader>
-      {resourceGroups.length === 0 && <Heading>Coming soon!</Heading>}
-      {resourceGroups.length > 0 && (
-        <Tabs variant="enclosed">
-          <TabList>
-            {resourceGroups.map((rg) => (
-              <Tab key={rg.id}>{rg.title}</Tab>
-            ))}
-          </TabList>
-          <TabPanels>
-            {resourceGroups.map((rg) => (
-              <TabPanel key={rg.id}>
-                <Stack spacing={3}>
-                  {rg.resources.map((r) => (
-                    <Box>
-                      <a href={r.url} rel="noopener noreferrer" target="_blank">
-                        <TextButton>{r.title}</TextButton>
-                      </a>
-                    </Box>
-                  ))}
-                </Stack>
-              </TabPanel>
-            ))}
-          </TabPanels>
-        </Tabs>
-      )}
+      <Stack spacing={16}>
+        {resourceGroups.length === 0 && <Heading>Coming soon!</Heading>}
+        {resourceGroups.length > 0 && (
+          <Tabs variant="enclosed">
+            <TabList>
+              {resourceGroups.map((rg) => (
+                <Tab key={rg.id}>{rg.title}</Tab>
+              ))}
+            </TabList>
+            <TabPanels>
+              {resourceGroups.map((rg) => (
+                <TabPanel key={rg.id}>
+                  <Stack spacing={3}>
+                    {rg.resources.map((r) => (
+                      <Box>
+                        <a href={r.url} rel="noopener noreferrer" target="_blank">
+                          <TextButton>{r.title}</TextButton>
+                        </a>
+                      </Box>
+                    ))}
+                  </Stack>
+                </TabPanel>
+              ))}
+            </TabPanels>
+          </Tabs>
+        )}
+        <Box>
+          <a href="https://bestsummercamps.co/camps/apex/" rel="noopener noreferrer" target="_blank">
+            <TextButton>Featured on Best Summer Camps – Apex Summer Camps Guide</TextButton>
+          </a>
+        </Box>
+      </Stack>
     </BodyContainer>
   );
 };
