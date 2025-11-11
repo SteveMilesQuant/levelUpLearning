e0f95aa58938060d7b24b36b121cba24426348d3
Steve Miles <steven.miles.quant@gmail.com>
2025-11-10 20:28:56 -0500

    fixes

--- bin/launchec2.sh
+++ bin/launchec2.sh
@@ -1,10 +1,15 @@
 REGION=us-east-2
-AMI_ID=$(aws ssm get-parameters --names "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2" --region $REGION --query "Parameters[0].Value" --output text)
-echo "AMI_ID=$AMI_ID"
+# https://cloud-images.ubuntu.com/locator/ec2/
+AMI_ID=ami-0f5fcdfbd140e4ab7
+
+
 INSTANCE_ID=$(aws ec2 run-instances --image-id "$AMI_ID" --count 1 --instance-type t3.micro --key-name lul --security-group-ids "$SEC_GROUP_ID" --region $REGION --query "Instances[0].InstanceId" --output text)
 echo "INSTANCE_ID=$INSTANCE_ID"
+jq --arg id "$INSTANCE_ID" '.awsEc2InstanceId = $id' .vscode/settings.json > .vscode/settings.tmp && mv .vscode/settings.tmp .vscode/settings.json
+
 IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
 echo "IP_ADDRESS=$IP_ADDRESS"
+jq --arg id "$IP_ADDRESS" '.awsEc2InstanceIPAddress = $id' .vscode/settings.json > .vscode/settings.tmp && mv .vscode/settings.tmp .vscode/settings.json
 
 REG_TOKEN=$(curl -s -X POST \
   -H "Authorization: token $GITHUB_PAT" \
@@ -33,9 +38,10 @@ while true; do
   fi
 done
 
-scp -o StrictHostKeyChecking=no -i ~/.ssh/lul.pem -r ./api/.env ec2-user@$IP_ADDRESS:/home/ec2-user/lul.env
-scp -o StrictHostKeyChecking=no -i ~/.ssh/lul.pem -r ./scripts ec2-user@$IP_ADDRESS:/home/ec2-user/lul-scripts
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/lul.pem ec2-user@$IP_ADDRESS "REG_TOKEN=$REG_TOKEN bash -s" <<'EOF'
+scp -o StrictHostKeyChecking=no -i ~/.ssh/lul.pem -r ./api/.env ubuntu@$IP_ADDRESS:/home/ubuntu/lul.env
+ssh -i ~/.ssh/lul.pem ubuntu@$IP_ADDRESS "rm -rf /home/ubuntu/lul-scripts"
+scp -o StrictHostKeyChecking=no -i ~/.ssh/lul.pem -r ./scripts ubuntu@$IP_ADDRESS:/home/ubuntu/lul-scripts
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/lul.pem ubuntu@$IP_ADDRESS "REG_TOKEN=$REG_TOKEN bash -s" <<'EOF'
 # Create runner directory
 mkdir -p actions-runner && cd actions-runner
 
@@ -48,13 +54,13 @@ tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
 ./config.sh --url https://github.com/SteveMilesQuant/levelUpLearning --token $REG_TOKEN --labels new-runner,self-hosted --unattended
 
 # Install as service and start
-sudo cp /home/ec2-user/lul-scripts/github-runner.service /etc/systemd/system/github-runner.service
+sudo cp /home/ubuntu/lul-scripts/github-runner.service /etc/systemd/system/github-runner.service
 sudo systemctl daemon-reload
 sudo systemctl enable github-runner.service
 sudo systemctl start github-runner.service
 EOF
 
-jq --arg id "$INSTANCE_ID" '.awsEc2InstanceId = $id' .vscode/settings.json > .vscode/settings.tmp && mv .vscode/settings.tmp .vscode/settings.json
-jq --arg id "$IP_ADDRESS" '.awsEc2InstanceIPAddress = $id' .vscode/settings.json > .vscode/settings.tmp && mv .vscode/settings.tmp .vscode/settings.json
+
+
 
 
