e661c04e8dfd4d85e19a83d686e6b5a3f7acfaea
Steve Miles <steven.miles.quant@gmail.com>
2026-01-30 18:30:48 -0500

    change cert check to health check (with disk usage)

--- api/certcheck.py
+++ api/certcheck.py
@@ -7,20 +7,30 @@ from enrollment import CONFIRMATION_SENDER_EMAIL_KEY
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import asyncio
+from enum import IntEnum
 
-CERTGOOD_EMAIL_TEMPLATE = '''Hello LUL owners,
 
-Your certification is still valid and expires on {expiry}. Once we are within 30 days, we will renew.
+class Health(IntEnum):
+    GOOD = 0
+    FINE = 1
+    POOR = 2
 
-Sincerely,
+    def to_string(self) -> str:
+        return self.name
 
-Your email bot
-'''
+    def to_html(self) -> str:
+        color = {
+            Health.GOOD: "green",
+            Health.FINE: "orange",
+            Health.POOR: "red",
+        }[self]
+        return f'<span style="color: {color};">{self.name}</span>'
 
-CERTEXPIRED_EMAIL_TEMPLATE = '''Hello LUL owners,
 
-Your certification expires soon on {expiry}. Steve should renew by running the "Post-routing steps" from github.
+HEALTHCHECK_EMAIL_TEMPLATE = '''Hello LUL owners,
 
+Your technical health is {summary_status}. Individual checks:
+{health_check_list}
 Sincerely,
 
 Your email bot
@@ -38,7 +48,7 @@ def get_cert_expiry(cert_path):
     return expiry_date
 
 
-async def send_cert_expired_email(email_server: EmailServer, expiry: datetime):
+async def send_health_check_email(email_server: EmailServer, summary_status: str, health_check_list: str):
     sender_email = email_server.sender_emails.get(
         CONFIRMATION_SENDER_EMAIL_KEY)
     if sender_email is None:
@@ -46,38 +56,48 @@ async def send_cert_expired_email(email_server: EmailServer, expiry: datetime):
     message = MIMEMultipart("alternative")
     message.attach(
         MIMEText(
-            '<pre style="font-family: georgia,serif;">' + CERTEXPIRED_EMAIL_TEMPLATE.format(
-                expiry=expiry,
+            '<pre style="font-family: georgia,serif;">' + HEALTHCHECK_EMAIL_TEMPLATE.format(
+                summary_status=summary_status,
+                health_check_list=health_check_list,
             ) + '</pre>', 'html'
         )
     )
-    message["Subject"] = "Website certification check - SOON TO EXPIRE"
-    message["From"] = sender_email
-    message["Cc"] = sender_email
-    message["To"] = "steven.miles.quant@gmail.com"
-    await email_server.send_email(message)
-
-
-async def send_cert_not_expired_email(email_server: EmailServer, expiry: datetime):
-    sender_email = email_server.sender_emails.get(
-        CONFIRMATION_SENDER_EMAIL_KEY)
-    if sender_email is None:
-        return
-    message = MIMEMultipart("alternative")
-    message.attach(
-        MIMEText(
-            '<pre style="font-family: georgia,serif;">' + CERTGOOD_EMAIL_TEMPLATE.format(
-                expiry=expiry,
-            ) + '</pre>', 'html'
-        )
-    )
-    message["Subject"] = "Website certification check - VALID"
+    message["Subject"] = f"Technical health check - {summary_status}"
     message["From"] = sender_email
     message["Cc"] = sender_email
     message["To"] = "steven.miles.quant@gmail.com"
     await email_server.send_email(message)
 
 if __name__ == "__main__":
+    # Initiatlize overall status
+    summary_status = Health.GOOD
+    HEALTH_CHECKLIST = ""
+
+    # Check certification
+    expiry = get_cert_expiry("/etc/letsencrypt/fullchain.pem")
+    if expiry < datetime.now():
+        cert_status = Health.POOR
+    elif expiry < datetime.now() + timedelta(days=30):
+        cert_status = Health.FINE
+    else:
+        cert_status = Health.GOOD
+    HEALTH_CHECKLIST = HEALTH_CHECKLIST + \
+        f"   • Certification ({cert_status.to_html()}): Expires on {expiry}\n"
+    summary_status = max(summary_status, cert_status)
+
+    # Check hard drive space (passed in as env variable)
+    CHECK_DISK_PCT = int(os.getenv("CHECK_DISK_PCT", "500"))
+    if CHECK_DISK_PCT < 80:
+        disk_status = Health.GOOD
+    elif CHECK_DISK_PCT < 90:
+        disk_status = Health.FINE
+    else:
+        disk_status = Health.POOR
+    HEALTH_CHECKLIST = HEALTH_CHECKLIST + \
+        f"   • Server disk ({disk_status.to_html()}): {CHECK_DISK_PCT}% usage\n"
+    summary_status = max(summary_status, disk_status)
+
+    # Send email
     email_server = EmailServer(
         host=os.environ.get("SMTP_HOST", None),
         port=os.environ.get("SMTP_PORT", None),
@@ -86,11 +106,5 @@ if __name__ == "__main__":
         sender_emails={CONFIRMATION_SENDER_EMAIL_KEY: os.environ.get(
             "CONFIRMATION_EMAIL_SENDER", None)}
     )
-
-    expiry = get_cert_expiry("/etc/letsencrypt/fullchain.pem")
-    if expiry < datetime.now() + timedelta(days=30):
-        asyncio.run(send_cert_expired_email(email_server, expiry))
-        sys.exit(1)
-    else:
-        asyncio.run(send_cert_not_expired_email(email_server, expiry))
-        sys.exit(0)  # check with $?
+    asyncio.run(send_health_check_email(
+        email_server, summary_status.to_string(), HEALTH_CHECKLIST))
--- scripts/certcheck.sh
+++ scripts/certcheck.sh
@@ -1,1 +1,2 @@
-docker run --rm --env-file ~/lul.env -v "/usr/local/nginx/conf/leveluplearningnc.com:/etc/letsencrypt" --user $(id -u):$(id -g) lul-email python3 ./certcheck.py
+CHECK_DISK_PCT=$(df --output=pcent / | tail -n 1 | tr -dc '0-9')
+docker run --rm --env-file ~/lul.env -e CHECK_DISK_PCT="$CHECK_DISK_PCT" -v "/usr/local/nginx/conf/leveluplearningnc.com:/etc/letsencrypt" --user $(id -u):$(id -g) lul-email python3 ./certcheck.py
